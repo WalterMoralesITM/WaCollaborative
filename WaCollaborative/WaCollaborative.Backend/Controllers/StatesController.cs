16eff0a03e4e623fcf3f0e41dea2874bf8d4530c
Efrain Trujillo <etrujillomalaver22@hotmail.com>
2023-09-14 23:53:29 -0500

    add crud of states and cities

--- WaCollaborative/WaCollaborative.Backend/Controllers/StatesController.cs
+++ WaCollaborative/WaCollaborative.Backend/Controllers/StatesController.cs
@@ -1,6 +1,8 @@
 ﻿#region Using
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WaCollaborative.Backend.Data;
 using WaCollaborative.Backend.Interfaces;
 using WaCollaborative.Shared.Entities;
 
@@ -8,7 +10,6 @@ using WaCollaborative.Shared.Entities;
 
 namespace WaCollaborative.Backend.Controllers
 {
-
     /// <summary>
     /// The Controller StatesController
     /// </summary>
@@ -17,14 +18,43 @@ namespace WaCollaborative.Backend.Controllers
     [Route("api/[controller]")]
     public class StatesController : GenericController<State>
     {
+        #region Attributes
+
+        private readonly DataContext _context;
+
+        #endregion Attributes
 
         #region Constructor
 
-        public StatesController(IGenericUnitOfWork<State> unitOfWork) : base(unitOfWork)
+        public StatesController(IGenericUnitOfWork<State> unitOfWork, DataContext context) : base(unitOfWork)
         {
+            _context = context;
         }
 
         #endregion Constructor
 
+        #region Methods
+
+        [HttpGet]
+        public override async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.States.Include(e => e.Cities).ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public override async Task<IActionResult> GetAsync(int id)
+        {
+            var state = await _context.States
+                 .Include(x => x.Cities)
+                 .FirstOrDefaultAsync(x => x.Id == id);
+            if (state is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(state);
+        }
+
+        #endregion Methods
     }
 }

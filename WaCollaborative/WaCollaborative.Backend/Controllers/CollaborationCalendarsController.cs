8cc6e3f5971879f16a4079a01beef2d67ce51083
Efrain Trujillo <etrujillomalaver22@hotmail.com>
2023-11-15 16:40:25 -0500

    add collaboration calendar

--- WaCollaborative/WaCollaborative.Backend/Controllers/CollaborationCalendarsController.cs
+++ WaCollaborative/WaCollaborative.Backend/Controllers/CollaborationCalendarsController.cs
@@ -1,17 +1,84 @@
-﻿using WaCollaborative.Backend.Data;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WaCollaborative.Backend.Data;
 using WaCollaborative.Backend.Interfaces;
+using WaCollaborative.Shared.DTOs;
 using WaCollaborative.Shared.Entities;
+using WaCollaborative.Shared.Helpers;
 
 namespace WaCollaborative.Backend.Controllers
 {
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("api/[controller]")]
     public class CollaborationCalendarsController : GenericController<CollaborationCalendar>
     {
         private readonly DataContext _context;
+        private readonly IGenericUnitOfWork<CollaborationCalendar> _unitOfWork;
 
         public CollaborationCalendarsController(IGenericUnitOfWork<CollaborationCalendar> unitOfWork, DataContext context) 
             : base(unitOfWork, context)
         {
             _context = context;
+            _unitOfWork = unitOfWork;
         }
+
+        [AllowAnonymous]
+        [HttpGet("combo")]
+        public async Task<ActionResult> GetComboAsync()
+        {
+            return Ok(await _context.CollaborationCalendars
+                .OrderBy(p => p.StartDate)
+                .OrderBy(p => p.EndDate)
+                .ToListAsync());
+        }
+
+        [HttpGet]
+        public override async Task<IActionResult> GetAsync([FromQuery] PaginationDTO pagination)
+        {
+            var queryable = _context.CollaborationCalendars
+                 .AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(pagination.Filter))
+            {
+                queryable = queryable.Where(x => x.InternalRole!.Name.Contains(pagination.Filter));
+            }
+
+            return Ok(await queryable.Include(c => c.InternalRole)
+                .OrderBy(p => p.StartDate)
+                .OrderBy(p => p.EndDate)
+                .Paginate(pagination)
+                .ToListAsync());
+        }
+
+        [HttpGet("totalPages")]
+        public override async Task<ActionResult> GetPagesAsync([FromQuery] PaginationDTO pagination)
+        {
+            var queryable = _context.CollaborationCalendars.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(pagination.Filter))
+            {
+                queryable = queryable.Where(x => x.InternalRole!.Name.Contains(pagination.Filter));
+            }
+
+            double count = await queryable.CountAsync();
+            double totalPages = Math.Ceiling(count / pagination.RecordsNumber);
+            return Ok(totalPages);
+        }
+
+        [HttpGet("{id}")]
+        public override async Task<IActionResult> GetAsync(int id)
+        {
+            var product = await _context.CollaborationCalendars
+                 .FirstOrDefaultAsync(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
     }
 }
